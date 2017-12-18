14e2b566c9d8c31dd9be1a78954f552f7f1d62b8
Lenin Meza <lmeza@anzen.com.mx>
2017-12-18 12:00:17 -0600

    :rotating_light: feat(eslint): Removed ESLint warnings

--- index.js
+++ index.js
@@ -9,22 +9,23 @@ let dates = [];
 
 for (let i = 0; i < 10; i++) {
     const date = moment()
-    .add(i,'d');
+    .add(i, 'd');
     dates.push({
         nombreFecha: `Fec${i + 1}`,
-        //fecha: `Fri Dec ${i} 12:25:02 CST ${currentYear}`,
         fecha: new Date(date).toString(),
     });
 }
 // Put the dates in the response
 const file = './data.json';
 const response = {
-    "codigo": "200",
-    "data": dates,
+    'codigo': '200',
+    'data': dates,
 };
 const spaces = {
-    spaces: 2
+    spaces: 2,
 };
+
+// Write the JSON File
 jsonfile.writeFile(file, response, spaces, function(err) {
-    if (err) throw `Error: ${err}`;
+    if (err) throw err;
 });
